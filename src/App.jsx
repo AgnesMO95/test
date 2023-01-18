298b2c2cd2fc89f9c889d085686f0cdc691d55d7
agnesockernahl <agnes.ockernahl@payex.com>
2023-01-18 15:48:23 +0100

    slettet styling filer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
 import Restaurants from "./Restaurants";
 
 function App() {
--- src/Restaurants.jsx
+++ src/Restaurants.jsx
@@ -1,6 +1,4 @@
 import { useEffect, useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
 import Restaurant from "./Restaurant";
 
 function Restaurants() {
